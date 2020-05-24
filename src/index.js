18e122513e369895d359217d26d31fddd648137e
SerdzhyoVashchuk <serj.vaschuk@gmail.com>
2020-05-25 00:20:31 +0300

    add directoty src

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './style/style.sass'
+// import img from './img/logo-default-261x72.png'
+// import img from './img/header/logo-default-261x72.png'
+// import ~normalize.scss  подключать в css // ~ (алиас) импорт из node_modules
+
+//json без модулей импортируется
