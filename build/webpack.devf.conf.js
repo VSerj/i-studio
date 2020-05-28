64caf2421fa9a570e4b908ae5694bc5fd3ac1381
dedaSerj <serj.vaschuk@gmail.com>
2020-05-28 21:20:13 +0300

    added webpack conf

---
+++ build/webpack.devf.conf.js
@@ -0,0 +1,28 @@
+const webpack =  require('webpack')
+const merge = require('webpack-merge')
+const baseWebpackConfig = require('./webpack.base.conf')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+const devWebpackConfig = merge(baseWebpackConfig, {
+  // DEV config
+  mode: 'development',
+  devtool: 'cheap-module-eval-source-map',
+  // devServer: {
+  //   contentBase: baseWebpackConfig.externals.paths.dist,
+  //   port: 4200,
+  //   overlay: {
+  //     warnings: true,
+  //     errors: true
+  //   }
+  // },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.SourceMapDevToolPlugin({
+      filename: '[file].map'
+    })
+  ]
+})
+
+module.exports = new Promise((resolve, reject) => {
+  resolve(devWebpackConfig)
+})
