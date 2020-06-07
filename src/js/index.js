6243cad45049fc152feeca573e854795ea8c6df5
dedaSerj <serj.vaschuk@gmail.com>
2020-06-07 22:32:41 +0300

    add script for body (lock scroll)

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,33 @@
 'use strict'
 
-document.querySelector('.menu_btn').addEventListener('click',showMenu);
+document.querySelector('.menu_btn').addEventListener('click',showMenu)
 
 function showMenu() {
-  document.querySelector('.header_nav').classList.toggle('active_menu');
+  document.querySelector('.header_nav').classList.toggle('active_menu')
 }
+
+let currentElem = null;
+
+document.addEventListener('mouseover', function(e) {
+  let target = e.target.closest('.active_menu')
+
+  if(!target) return
+  
+  currentElem = target
+  document.body.classList.add('lock_flow')
+})
+
+document.addEventListener('mouseout', function(e) {
+  if(!currentElem) return
+
+  let relatedTarget = e.relatedTarget
+
+  while(relatedTarget) {
+    if (relatedTarget == currentElem) return
+    relatedTarget = relatedTarget.parentNode
+  }
+
+  document.body.classList.remove('lock_flow')
+  currentElem = null;
+
+})
