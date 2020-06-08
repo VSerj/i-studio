ebebc95e7f22feae85d216fc144efe0cfd09e365
dedaSerj <serj.vaschuk@gmail.com>
2020-06-08 18:12:03 +0300

    add animation for menu_btn (burget to arrow)

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,54 @@
 'use strict'
+let menuButton = document.querySelector('.menu_btn')
 
-document.querySelector('.menu_btn').addEventListener('click',showMenu)
+menuButton.addEventListener('click',useNavigation)
 
-function showMenu() {
-  document.querySelector('.header_nav').classList.toggle('active_menu')
+function useNavigation() {
+
+  useAnimationForBurger()
+  showHideNavigation()
+
+}
+
+function useAnimationForBurger() {
+
+  menuButton.classList.toggle('is_active_menu')
+
+}
+
+function showHideNavigation() {
+
+  document.querySelector('.header_nav').classList.toggle('is_active_menu')
+
 }
 
 let currentElem = null;
 
 document.addEventListener('mouseover', function(e) {
-  let target = e.target.closest('.active_menu')
+
+  let target = e.target.closest('.is_active_menu')
 
   if(!target) return
-  
+
   currentElem = target
   document.body.classList.add('lock_flow')
+
 })
 
 document.addEventListener('mouseout', function(e) {
+
   if(!currentElem) return
 
   let relatedTarget = e.relatedTarget
 
   while(relatedTarget) {
+
     if (relatedTarget == currentElem) return
     relatedTarget = relatedTarget.parentNode
+    
   }
 
   document.body.classList.remove('lock_flow')
-  currentElem = null;
+  currentElem = null
 
 })
