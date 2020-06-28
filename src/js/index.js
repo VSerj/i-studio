1f835f27e7b7f1294819ba4cce53f1c780c34bbd
dedaSerj <serj.vaschuk@gmail.com>
2020-06-28 12:06:07 +0300

    automatic changes to package-lock.json

--- src/js/index.js
+++ src/js/index.js
@@ -1,54 +1,53 @@
 'use strict'
 let menuButton = document.querySelector('.menu_btn')
 
-menuButton.addEventListener('click',useNavigation)
+menuButton.addEventListener('click', useNavigation)
 
 function useNavigation() {
-
   useAnimationForBurger()
   showHideNavigation()
-
 }
 
 function useAnimationForBurger() {
-
   menuButton.classList.toggle('is_active_menu')
-
 }
 
 function showHideNavigation() {
-
   document.querySelector('.header_nav').classList.toggle('is_active_menu')
-
 }
 
-let currentElem = null;
+// window.addEventListener('scroll', function(e) {
+//   if (window.pageYOffset > 10) {
+//     document.querySelector(`.header`).classList
+//   }
+// })
+// let currentElem = null;
 
-document.addEventListener('mouseover', function(e) {
+// document.addEventListener('mouseover', function(e) {
 
-  let target = e.target.closest('.is_active_menu')
+//   let target = e.target.closest('.is_active_menu')
 
-  if(!target) return
+//   if(!target) return
 
-  currentElem = target
-  document.body.classList.add('lock_flow')
+//   currentElem = target
+//   document.body.classList.add('lock_flow')
 
-})
+// })
 
-document.addEventListener('mouseout', function(e) {
+// document.addEventListener('mouseout', function(e) {
 
-  if(!currentElem) return
+//   if(!currentElem) return
 
-  let relatedTarget = e.relatedTarget
+//   let relatedTarget = e.relatedTarget
 
-  while(relatedTarget) {
+//   while(relatedTarget) {
 
-    if (relatedTarget == currentElem) return
-    relatedTarget = relatedTarget.parentNode
+//     if (relatedTarget == currentElem) return
+//     relatedTarget = relatedTarget.parentNode
     
-  }
+//   }
 
-  document.body.classList.remove('lock_flow')
-  currentElem = null
+//   document.body.classList.remove('lock_flow')
+//   currentElem = null
 
-})
+// })
