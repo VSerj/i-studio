fea866529b0d4fedf44a09703fea42be59af0732
SerdzhyoVashchuk <serj.vaschuk@gmail.com>
2020-08-31 00:07:30 +0300

    add script for header && edit transition

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,4 +1,28 @@
-'use strict'
-window.addEventListener('scroll', function(el) {
+"use strict"
 
-});
+let header = {
+  elem: document.querySelector("header"),
+  selectorLowerHeight: "header--lower",
+  addLowerHeight: function () {
+    this.elem.classList.add(this.selectorLowerHeight)
+  },
+  removeLowerHeight: function () {
+    this.elem.classList.remove(this.selectorLowerHeight)
+  },
+  checkScrolling: isWindowScrolling,
+  changeHeightWhenScroliing: function () {
+    if (this.checkScrolling()) {
+      this.addLowerHeight()
+    } else {
+      this.removeLowerHeight()
+    }
+  },
+}
+
+header.changeHeightWhenScroliing()
+
+window.addEventListener("scroll", header.changeHeightWhenScroliing.bind(header))
+
+function isWindowScrolling() {
+  return !!window.scrollY
+}
