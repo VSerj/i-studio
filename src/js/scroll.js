3279339fc3bab1864c44774e2ace2b7337ecc3b3
SerdzhyoVashchuk <serj.vaschuk@gmail.com>
2020-09-06 20:25:30 +0300

    rework header js

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,28 +1,19 @@
-"use strict"
+'use strict'
 
-let header = {
-  elem: document.querySelector("header"),
-  selectorLowHeight: "header--lower",
-  addLowHeight: function () {
-    this.elem.classList.add(this.selectorLowHeight)
-  },
-  removeLowHeight: function () {
-    this.elem.classList.remove(this.selectorLowHeight)
-  },
-  checkScrolling: isWindowScrolling,
-  changeHeightWhenScroliing: function () {
-    if (this.checkScrolling()) {
-      this.addLowHeight()
-    } else {
-      this.removeLowHeight()
-    }
-  },
+const header = document.querySelector('header')
+const headerStretch = 'header--stretch'
+
+function changeHeaderHeight() {
+  if (!window.scrollY && !checkStretch()) {
+    header.classList.add(headerStretch)
+  } 
+  else if (checkStretch()) {
+    header.classList.remove(headerStretch)
+  }
+
+  function checkStretch() {
+    return header.classList.contains(headerStretch)
+  }
 }
 
-header.changeHeightWhenScroliing()
-
-window.addEventListener("scroll", header.changeHeightWhenScroliing.bind(header))
-
-function isWindowScrolling() {
-  return !!window.scrollY
-}
+window.addEventListener('scroll', changeHeaderHeight)
