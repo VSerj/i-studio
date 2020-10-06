11c790cb77950c2ec08f9126f484642ff8ddc01a
SerdzhyoVashchuk <serj.vaschuk@gmail.com>
2020-10-07 01:51:13 +0300

    del extra code

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -5,7 +5,7 @@ const headerStretch = 'header--stretch'
 const observerElems = document.querySelectorAll('.fade')
 
 window.addEventListener('load', () => {
-  console.log('ready');
+
   addObserver(observerElems)
   
 })
@@ -25,12 +25,10 @@ let observer = new IntersectionObserver( (entries, observer) => {
     if (entry.isIntersecting) {
       
       entry.target.classList.add('rollUp')
-      // entry.target.classList.contains('[data-t-delay]') ? 
 
       observer.unobserve(entry.target)
 
     }
-
 
   })
 
@@ -46,68 +44,4 @@ function addObserver(observerList) {
 
 }
 
-// // window.addEventListener('DOMContentLoaded', throttleScroll)
-// // window.addEventListener('scroll', throttleScroll)
-
-// // function throttleScroll() {
-
-// //   window.requestAnimationFrame(function() {
-// //     changeHeaderHeight()
-// //     moveItems()
-// //   })
-
-// // }
-
-// // function changeHeaderHeight() {
-
-// //   if (!window.scrollY && !checkStretch()) {
-// //     header.classList.add(headerStretch)
-// //   }
-
-// //   if (window.scrollY && checkStretch()) {
-// //     header.classList.remove(headerStretch)
-// //   }
-
-// //   function checkStretch() {
-// //     return header.classList.contains(headerStretch)
-// //   }
-
-// // }
-
-// // function moveItems() {
-
-// //   function isPartiallyVisible(el) {
-
-// //     let elementBoundary = el.getBoundingClientRect()
-
-// //     let top = elementBoundary.top
-// //     let bottom = elementBoundary.bottom
-
-// //     return top + 10 < document.documentElement.clientHeight && bottom - header.clientHeight - 10 > 0
-
-// //   }
-
-// //   listMovingItems.forEach(el => {
-
-// //     if (isPartiallyVisible(el)) {
-
-// //       switch (el.dataset.moveSide) {
-
-// //         case 'right':
-// //           el.classList.add('move-to-right')
-// //           break
-
-// //         case 'left':
-// //           el.classList.add('move-to-left')
-// //           break
-
-// //         case 'top':
-// //           el.classList.add('move-to-top')
-// //           break
-
-// //       }
-// //     }
-
-// //   });
-// // }
 
