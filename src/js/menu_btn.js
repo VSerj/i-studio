7127b6e99ac0e93a4dabc57345794977fd3f59cb
SerdzhyoVashchuk <serj.vaschuk@gmail.com>
2020-10-07 01:46:04 +0300

    edit scroll animation: del old code && add observer && css animation classes

--- src/js/menu_btn.js
+++ src/js/menu_btn.js
@@ -6,14 +6,19 @@ let nav = document.querySelector('.nav')
 document.addEventListener('click', function (e) {
 
   if (e.target.closest('.btn-burger')) {
+
     burger.classList.toggle('is_active_menu')
     nav.classList.toggle('is_active_menu')
+
   }
 
   if (!e.target.closest('.nav-panel-mobile') && !e.target.closest('.nav')) {
+
     burger.classList.remove('is_active_menu')
     nav.classList.remove('is_active_menu')
+
   }
   
 })
+
 
--- src/js/scroll.js
+++ src/js/scroll.js
@@ -2,70 +2,112 @@
 
 const header = document.querySelector('header')
 const headerStretch = 'header--stretch'
-const listMovingItems = document.querySelectorAll('[data-move-side]')
+const observerElems = document.querySelectorAll('.fade')
 
-window.addEventListener('DOMContentLoaded', throttleScroll)
-window.addEventListener('scroll', throttleScroll)
+window.addEventListener('load', () => {
+  console.log('ready');
+  addObserver(observerElems)
+  
+})
 
-function throttleScroll() {
+let optionsObs = {
 
-  window.requestAnimationFrame(function() {
-    changeHeaderHeight()
-    moveItems()
-  })
-
-}
-
-function changeHeaderHeight() {
-
-  if (!window.scrollY && !checkStretch()) {
-    header.classList.add(headerStretch)
-  }
-
-  if (window.scrollY && checkStretch()) {
-    header.classList.remove(headerStretch)
-  }
-
-  function checkStretch() {
-    return header.classList.contains(headerStretch)
-  }
+  root: null,
+  rootMargin: '-100px 0px -30px 0px',
+  threshold: 0
 
 }
 
-function moveItems() {
+let observer = new IntersectionObserver( (entries, observer) => {
 
-  function isPartiallyVisible(el) {
+  entries.forEach( (entry) => {
 
-    let elementBoundary = el.getBoundingClientRect()
+    if (entry.isIntersecting) {
+      
+      entry.target.classList.add('rollUp')
+      // entry.target.classList.contains('[data-t-delay]') ? 
 
-    let top = elementBoundary.top
-    let bottom = elementBoundary.bottom
+      observer.unobserve(entry.target)
 
-    return top + 10 < document.documentElement.clientHeight && bottom - header.clientHeight - 10 > 0
-
-  }
-
-  listMovingItems.forEach(el => {
-
-    if (isPartiallyVisible(el)) {
-
-      switch (el.dataset.moveSide) {
-
-        case 'right':
-          el.classList.add('move-to-right')
-          break
-
-        case 'left':
-          el.classList.add('move-to-left')
-          break
-
-        case 'top':
-          el.classList.add('move-to-top')
-          break
-
-      }
     }
 
+
+  })
+
+}, optionsObs)
+
+function addObserver(observerList) {
+
+  observerList.forEach(el => {
+
+    observer.observe(el)
+
   });
+
 }
+
+// // window.addEventListener('DOMContentLoaded', throttleScroll)
+// // window.addEventListener('scroll', throttleScroll)
+
+// // function throttleScroll() {
+
+// //   window.requestAnimationFrame(function() {
+// //     changeHeaderHeight()
+// //     moveItems()
+// //   })
+
+// // }
+
+// // function changeHeaderHeight() {
+
+// //   if (!window.scrollY && !checkStretch()) {
+// //     header.classList.add(headerStretch)
+// //   }
+
+// //   if (window.scrollY && checkStretch()) {
+// //     header.classList.remove(headerStretch)
+// //   }
+
+// //   function checkStretch() {
+// //     return header.classList.contains(headerStretch)
+// //   }
+
+// // }
+
+// // function moveItems() {
+
+// //   function isPartiallyVisible(el) {
+
+// //     let elementBoundary = el.getBoundingClientRect()
+
+// //     let top = elementBoundary.top
+// //     let bottom = elementBoundary.bottom
+
+// //     return top + 10 < document.documentElement.clientHeight && bottom - header.clientHeight - 10 > 0
+
+// //   }
+
+// //   listMovingItems.forEach(el => {
+
+// //     if (isPartiallyVisible(el)) {
+
+// //       switch (el.dataset.moveSide) {
+
+// //         case 'right':
+// //           el.classList.add('move-to-right')
+// //           break
+
+// //         case 'left':
+// //           el.classList.add('move-to-left')
+// //           break
+
+// //         case 'top':
+// //           el.classList.add('move-to-top')
+// //           break
+
+// //       }
+// //     }
+
+// //   });
+// // }
 
