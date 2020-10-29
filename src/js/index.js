326b9c3e602dc3b0081062a62c42045a47c1a9b0
SerdzhyoVashchuk <serj.vaschuk@gmail.com>
2020-10-29 23:58:09 +0200

    edit sass for slider && add minimal script for slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,8 @@
-// import './rollout_fade_effect.js'
 import { runBurger } from './menu_btn.js'
 import { rollOut } from './rollout_fade_effect.js'
 import { stretchHeader } from './stretch_header.js'
 import { runButtonUp } from './button_up.js'
 import { runSlider } from './slider'
-import './slider'
 
 window.addEventListener('load', () => {
   stretchHeader()
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,136 +1,105 @@
 'use strict'
 
 export const runSlider = () => {
+  //temp
   const slider = document.querySelector('.slider')
   const slides = document.querySelectorAll('.slider__item')
+  const currentSlide = document.querySelector('.slider__item--active')
   const nextBtn = document.querySelector('.slider-btn__next')
   const btnPrev = document.querySelector('.slider-btn__previous')
   const activeSlide = document.querySelector('.slider-item--active')
 
   const setting = {
+    infinite: true,   //зацикленный
+    auto: true,
+    slidesInArea: 2,  //колисество слайдов в области просмотра
+    slideWidth: 70,   //проценты
+    activeSelector: 'slider__item--active',
+  }
+
+  const sliderElems = {
+    slider,
+    slides,
     btnPrev,
     nextBtn,
-    infinite: true,   //зацикленный
-    toogles: true,    //для переключателей
-    visibleSlide: 2,  //колисество слайдов в области просмотра
+    currentSlide,
   }
 
   class Slider {
-    constructor(sliderArea, slideList, options) {
-      this.slider = sliderArea;
-      this.slides = slideList;
+    constructor(sliderElems, options) {
+      this.sliderElems = sliderElems;
+
+      this.slider = sliderElems.slider;
+      this.slides = sliderElems.slides;
+      this.currentSlide = sliderElems.currentSlide;
+      this.nextBtn = this.sliderElems.nextBtn;
+      this.prevBtn = this.sliderElems.btnPrev;
+      this.numberOfSlides = this.slides.length || 0; //количество слайдов
+
       this.options = options;
 
-      this.nextBtn = options.nextBtn;
-      this.btnPrev = options.btnPrev;
-      this.infinite = options.infinite;
-      this.toogles = options.toogles;
-      this.visibleSlide = options.visibleSlide
+      this.infinite = this.options.infinite;
+      this.auto = this.options.auto;
+      this.slidesInArea = this.options.slidesInArea || 0;
+      this.slideWidth = this.options.slideWidth;
+      this.activeSelector = this.options.activeSelector
 
-      this.activeSlide = 1;
+      this.currentIndex = 0;
+      this.currentShiftTranslate = 0; //shift transform: translateX
     }
 
-    next() {
-
+    move(step) {
+      return this.slider.style.transform = `translateX(${step}%)`
     }
 
-    previous() {
+    setActiveSlide() {
+      if (this.currentSlide.matches(`.${this.activeSelector}`)) {
+        this.currentSlide.classList.remove(this.activeSelector)
+      }
 
+      this.slides[this.currentIndex].classList.add(this.activeSelector)
+      this.currentSlide = this.slides[this.currentIndex]
+    }
+
+    nextSlide() {
+      const endBoundaryIndex = this.numberOfSlides - this.slidesInArea
+      if (this.currentIndex > endBoundaryIndex) {
+        return console.log('Листайте в другую сторону'); //temp
+      }
+
+      this.currentShiftTranslate += this.slideWidth * -1
+      this.move(this.currentShiftTranslate)
+
+      this.currentIndex++
+
+      this.setActiveSlide()
+    }
+
+    previousSlide() {
+      const startBoundaryIndex = 0
+      if (this.currentIndex <= startBoundaryIndex) {
+        return console.log('Листайте в другую сторону'); //temp
+      }
+
+      this.currentShiftTranslate += this.slideWidth
+      this.move(this.currentShiftTranslate)
+
+      this.currentIndex--
+
+      this.setActiveSlide()
     }
 
     test() {
-      console.log(this.visibleSlide);
+      // console.log(this.currentSlide.matches(`.${this.activeSelector}`))
     }
 
+    run() {
+      this.nextBtn.addEventListener('click', this.nextSlide.bind(this))
+      this.prevBtn.addEventListener('click', this.previousSlide.bind(this))
+    }
   }
 
-  const projectSlider = new Slider(slider, slides, setting)
+  const projectSlider = new Slider(sliderElems, setting)
+  projectSlider.run()
   projectSlider.test()
 }
-
-//1) При нажатии на кнопку влево/право идет смена 2ох картинок в области
-//   просмотра слайдера
-//2) Идет смена картинки 70% процентов от области просмотра и картинки - 30%
-//3) Если картинки сменились - необходимо установить(запомн.) позицию(индекс)
-//   текущей картинки (70%) для мобильного слайдера с переключателями
-//4) 
-
-
-// const email = document.querySelector('input[type="email"]')
-// // const emailValidation = new Validadaror(email, setting)
-
-// const setting = {
-//   rules: {
-//     min: 8,
-//     max: 50,
-//     match: 'email',
-//   },
-//   message: {
-//     min: 'Поле должно содержать больше 8 символов',
-//     max: 'Поле не должно содержать больше 8 символов',
-//     match: 'Поле должно содержать валидный адрес е.почты',
-//   },
-//   onError: function() {
-//     console.log('не успешная валидация');
-//   },
-//   onSuccess: function() {
-//     console.log('успешная валидация');
-//   }
-// }
-
-// const regExps = {
-//   email: /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i,
-//   url: /^((https?):\/\/(\w+:{0,1}\w*@)?(\S+)|)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/,
-//   numbers: /^\d+(\.\d{1,2})?$/,
-//   digits: /[0-9]*$/,
-//   letters: /[a-z][A-Z]*$/
-// }
-
-// class Validator {
-//   constructor(elem, options) {
-//     this.elem = elem,
-//     this.options = options
-
-//     this.value = this.elem.value.trim();
-//     this.length = this.value.length;
-//     this.rules = this.options.rules;
-//     this.mesages = this.options.messages;
-//   }
-
-//   required() {
-//     return this.length > 0;
-//   }
-
-//   min(param) {
-//     this.length >= param
-//   }
-
-//   min(param) {
-//     this.length <= param
-//   }
-
-//   match(param) {
-//     return regExps[param].test(this.value)
-//   }
-
-//   validate(){
-//     this.value = this.element.value.trim()
-//     this.length = this.value.length
-
-//     let isValide = true
-//     for (let rule in this.rules) {
-//       let param = this.rules[rule]
-//       let result = this[rule](param)
-//     }
-
-//     if(!result) {
-//       isValide = fasle
-//       this.options.onError()
-//       break
-//     }
-    
-//     if(isValide) {
-//       this.option.onSuccess()
-//     }
-//   }
-// }
