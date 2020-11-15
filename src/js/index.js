a73d7aaa52d038f486963b4b3ad89e7a5fc3455e
Serhii Vashchuk <serj.vaschuk@gmail.com>
2020-11-15 07:21:20 +0200

    add click on toggle && fast touch swipe && add img && edit import/export gits status

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
-import { runBurger } from './menu_btn.js'
-import { rollOut } from './rollout_fade_effect.js'
-import { stretchHeader } from './stretch_header.js'
-import { runButtonUp } from './button_up.js'
-import { runSlider } from './slider'
+import { runBurger } from './menu_btn.js';
+import { rollOut } from './rollout_fade_effect.js';
+import { stretchHeader } from './stretch_header.js';
+import { runButtonUp } from './button_up.js';
+import { Slider } from './slider';
 
 window.addEventListener('load', () => {
   // stretchHeader()
@@ -12,5 +12,21 @@ window.addEventListener('load', () => {
   // )
   // runBurger()
   // runButtonUp()
-  runSlider()
-})
+  new Slider(
+    {
+      slider: document.querySelector('.slider__slideList'),
+      slides: document.querySelectorAll('.slider__item'),
+      prevBtn: document.querySelector('.slider-btn__previous'),
+      nextBtn: document.querySelector('.slider-btn__next'),
+      toggles: document.querySelectorAll('.slider-toggle'),
+      activeSlide: document.querySelector('.slider__item.slide--isActive'),
+      activeToggle: document.querySelector('.slider-toggle.slide--isActive'),
+    },
+    {
+      inVisibleArea: 2,
+      slideWidth: 70, //проценты от области слайдера
+      activeSelector: 'slide--isActive',
+    }
+  )
+  .run();
+});
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,264 +1,262 @@
 'use strict';
 
-export const runSlider = () => {
-  const setting = {
-    loop: true, //зацикленный
-    inVisibleArea: 2, //колисество слайдов в области просмотра
-    slideWidth: 70, //проценты от области слайдера
-    activeSelector: 'slide--isActive',
-  };
+export class Slider {
+  constructor(sliderElems, options) {
+    //Options
+    this.options = options;
+    this.inVisibleArea = this.options.inVisibleArea;
+    this.step = this.options.slideWidth;
+    this.activeSelector = this.options.activeSelector;
+    //DOM elements
+    this.sliderElems = sliderElems;
+    this.slider = sliderElems.slider;
+    this.slides = sliderElems.slides;
+    this.nextBtn = sliderElems.nextBtn;
+    this.prevBtn = sliderElems.prevBtn;
+    this.toggles = sliderElems.toggles;
+    this.activeSlide = sliderElems.activeSlide;
+    this.activeToggle = sliderElems.activeToggle;
+    this.helperLoopSlides = [...this.slides].splice(0, this.inVisibleArea);
+    this.activeToggleID = 1;
+    //Counters and boundary
+    this.amountSlides = sliderElems.slides.length;
+    this.counterShiftSlider = 0;
+    this.startBoundarySlider = 0;
+    this.endBoundarySlider = -this.amountSlides;
+    this.helperBoundary = -this.inVisibleArea;
+    //state
+    this.isTrottled = false;
+    this.isClickOnToggle = false;
+    //swipe
+    this.swipe = {
+      startX: 0,
+      deltaX: 0,
+      startTime: 0,
+      maxTime: 700,
+      minDistance: 40,
+      maxDistance: 420,
+    };
+  }
 
-  const sliderElems = {
-    slider: document.querySelector('.slider__slideList'),
-    slides: document.querySelectorAll('.slider__item'),
-    prevBtn: document.querySelector('.slider-btn__previous'),
-    nextBtn: document.querySelector('.slider-btn__next'),
-    toggles: document.querySelectorAll('.slider-toggle'),
-    activeSlide: document.querySelector('.slider__item.slide--isActive'),
-    activeToggle: document.querySelector('.slider-toggle.slide--isActive'),
-  };
+  setTranslateX(element, value) {
+    element.style.transform = `translateX(${value}%)`;
+  }
 
-  class Slider {
-    constructor(sliderElems, options) {
-      //Options
-      this.options = options;
-      this.inVisibleArea = this.options.inVisibleArea;
-      this.step = this.options.slideWidth;
-      this.activeSelector = this.options.activeSelector;
-      //DOM elements
-      this.sliderElems = sliderElems;
-      this.slider = sliderElems.slider;
-      this.slides = sliderElems.slides;
-      this.nextBtn = sliderElems.nextBtn;
-      this.prevBtn = sliderElems.prevBtn;
-      this.toggles = sliderElems.toggles;
-      this.activeSlide = sliderElems.activeSlide;
-      this.activeToggle = sliderElems.activeToggle;
-      this.helperCaruselSlides = [...this.slides].splice(0, this.inVisibleArea);
-      //Counters and boundary
-      this.amountSlides = sliderElems.slides.length;
-      this.counterShiftSlider = 0;
-      this.startBoudarySlider = 0;
-      this.endBoundarySlider = -this.amountSlides;
-      this.helperBoundary = -this.inVisibleArea;
-      //state
-      this.isTrottled = false;
-      this.isSwipe = true;
-      //swipe
-      this.swipe = {
-        startX: 0,
-        deltaX: 0,
-        startTime: 0,
-        maxTime: 300,
-        minDistance: 40,
-        maxDistance: 220,
-        prevBoundary: -36,
-        nextBoundary: 36,
-        widthSlider: this.slider.getBoundingClientRect().width,
-      };
+  moveSliderOneStep() {
+    const CURRENT_STEP = this.counterShiftSlider * this.step;
+    this.setTranslateX(this.slider, CURRENT_STEP);
+  }
+
+  removeTransitionSlider() {
+    //для непомітного зміщення слайдера в інший кінець
+    this.slider.classList.remove('slider__slideList--moveTransition');
+  }
+
+  addTransitionSlider() {
+    if (this.slider.closest('.slider__slideList--moveTransition')) return;
+    this.slider.classList.add('slider__slideList--moveTransition');
+  }
+
+  // якщо слайдер досягає заданих границі, то зміщуємо його до протилежної
+  // для зацикленності
+  loopMovingToOpositeBoundary(moveDirection) {
+    const boundary = this.getOpositeBoundary(moveDirection);
+    if (boundary === undefined) return;
+
+    this.removeTransitionSlider();
+    this.counterShiftSlider = boundary;
+    this.setTranslateX(this.slider, boundary * this.step);
+    this.helperLoopSlides.forEach((slide) => {
+      slide.style.transform = `translateX(${boundary * -100}%)`;
+    });
+  }
+
+  getOpositeBoundary(moveDirection) {
+    return moveDirection === undefined
+      ? undefined
+      : this.counterShiftSlider === this.startBoundarySlider &&
+        moveDirection === 'next'
+      ? this.endBoundarySlider
+      : this.counterShiftSlider === this.endBoundarySlider &&
+        moveDirection === 'prev'
+      ? this.startBoundarySlider
+      : undefined;
+  }
+
+  // Переміщення слайдів, яких не видно в полі зору, для компенсації пустоти
+  // на початку і кінці слайдера при повільному русі. Для зацикленості.
+  moveHelperLoopSlides(moveDirection) {
+    if (
+      this.counterShiftSlider < this.helperBoundary ||
+      this.counterShiftSlider === this.startBoundarySlider
+    ) {
+      return;
     }
 
-    setTranslateX(element, value) {
-      element.style.transform = `translateX(${value}%)`;
-    }
+    const TRANSLATEX_VALUE =
+      moveDirection === 'next' ? 0 : 100 * this.amountSlides;
+    const INDEX = Math.abs(this.counterShiftSlider) - 1;
+    this.setTranslateX(this.helperLoopSlides[INDEX], TRANSLATEX_VALUE);
+  }
 
-    moveSliderOneStep() {
-      const CURRENT_STEP = this.counterShiftSlider * this.step;
-      this.setTranslateX(this.slider, CURRENT_STEP);
-    }
+  loopMoving(moveDirection) {
+    this.loopMovingToOpositeBoundary(moveDirection);
+    this.moveHelperLoopSlides(moveDirection);
+  }
 
-    removeTransitionSlider() {
-      //для непомітного зміщення слайдера в інший кінець
-      this.slider.classList.remove('slider__slideList--moveTransition');
-    }
-    addTransitionSlider() {
-      if (this.slider.closest('.slider__slideList--moveTransition')) return;
-      this.slider.classList.add('slider__slideList--moveTransition');
-    }
+  calcIndexActiveSlide() {
+    return Math.abs(this.counterShiftSlider) ===
+      Math.abs(this.endBoundarySlider)
+      ? 0
+      : Math.abs(this.counterShiftSlider);
+  }
 
-    getOpossiteBoundary(moveDirection) {
-      return moveDirection === undefined
-        ? undefined
-        : this.counterShiftSlider === this.startBoudarySlider &&
-          moveDirection === 'next'
-        ? this.endBoundarySlider
-        : this.counterShiftSlider === this.endBoundarySlider &&
-          moveDirection === 'prev'
-        ? this.startBoudarySlider
-        : undefined;
-    }
+  setActiveSlideAndToggle(index) {
+    this.removeActiveSlideAndToggle();
+    this.activeSlide = this.slides[index];
+    this.activeSlide.classList.add(this.activeSelector);
+    this.activeToggle = this.toggles[index];
+    this.activeToggle.classList.add(this.activeSelector);
+    this.activeToggleID = +this.activeToggle.dataset.toggleId;
+  }
 
-    moveToOpossiteBoundary(moveDirection) {
-      const boundary = this.getOpossiteBoundary(moveDirection);
-      if (boundary === undefined) return;
+  removeActiveSlideAndToggle() {
+    this.activeToggle.classList.remove(this.activeSelector);
+    this.activeSlide.classList.remove(this.activeSelector);
+  }
 
+  switchSlide() {
+    this.setActiveSlideAndToggle(this.calcIndexActiveSlide());
+    if (this.isClickOnToggle) {
       this.removeTransitionSlider();
-      this.counterShiftSlider = boundary;
-      this.setTranslateX(this.slider, this.counterShiftSlider * this.step);
-      this.helperCaruselSlides.forEach((slide) => {
-        slide.style.transform = `translateX(${boundary * -100}%)`;
-      });
-    }
-
-    moveHelperCaruselSlides(moveDirection) {
-      if (
-        this.counterShiftSlider < this.helperBoundary ||
-        this.counterShiftSlider === this.startBoudarySlider
-      ) {
-        return;
-      }
-
-      const TRANSLATEX_VALUE =
-        moveDirection === 'next' ? 0 : 100 * this.amountSlides;
-      const INDEX = Math.abs(this.counterShiftSlider) - 1;
-      this.setTranslateX(this.helperCaruselSlides[INDEX], TRANSLATEX_VALUE);
-    }
-
-    carusel(moveDirection) {
-      this.moveToOpossiteBoundary(moveDirection);
-      this.moveHelperCaruselSlides(moveDirection);
-    }
-
-    calcIndexActiveSlide() {
-      return this.counterShiftSlider === this.endBoundarySlider
-        ? 0
-        : Math.abs(this.counterShiftSlider);
-    }
-
-    setActiveSlideAndToggle() {
-      this.removeActiveSlideAndToggle();
-      const index = this.calcIndexActiveSlide();
-      this.activeSlide = this.slides[index];
-      this.activeSlide.classList.add(this.activeSelector);
-      this.activeToggle = this.toggles[index];
-      this.activeToggle.classList.add(this.activeSelector);
-    }
-
-    removeActiveSlideAndToggle() {
-      this.activeToggle.classList.remove(this.activeSelector);
-      this.activeSlide.classList.remove(this.activeSelector);
-    }
-
-    nextSlide() {
-      this.carusel('next');
-      this.counterShiftSlider++;
-      this.setActiveSlideAndToggle();
-      //затримка - для непомітного виконання зміщеннь при carusel
-      this.addTransitionSlider();
-      setTimeout(() => {
-        this.moveSliderOneStep();
-      }, 50);
-    }
-
-    previousSlide() {
-      this.carusel('prev');
-      this.counterShiftSlider--;
-      this.setActiveSlideAndToggle();
+      this.moveSliderOneStep();
+    } else {
+      //затримка - для непомітного виконання зміщеннь при loop
       setTimeout(() => {
         this.addTransitionSlider();
         this.moveSliderOneStep();
       }, 50);
     }
+  }
 
-    //trottledSlideSwitch - затримка зміни слайда для уникнення дефектів.
-    //delayMs = duration в .slider__slideList--moveTransition.
-    trottledSlideSwitch(func, delayMs) {
-      if (this.isTrottled) return;
+  nextSlide() {
+    this.loopMoving('next');
+    this.counterShiftSlider++;
+    this.switchSlide();
+  }
 
-      func.call(this);
+  prevSlide() {
+    this.loopMoving('prev');
+    this.counterShiftSlider--;
+    this.switchSlide();
+  }
 
-      setTimeout(() => (this.isTrottled = false), delayMs);
-      this.isTrottled = true;
+  toggleMoving() {
+    this.toggles.forEach((toggle) => {
+      toggle.addEventListener('click', () => {
+        const selectedToggleID = +toggle.dataset.toggleId;
+        // різниця використовується для визначення напралення руху і
+        // кроків для зміщення
+        const deltaToggleID = selectedToggleID - this.activeToggleID;
+        if (deltaToggleID === 0) {
+          return;
+        }
+        // для запуску switchSlide без transiiton
+        this.isClickOnToggle = true;
+        // функція, яка імітує кліки і слугує для зміщення слайдера
+        // на к-ть кроків до відповідного слайдера, визваного перемикачем
+        const numberOfcallsToMove = (nameFuncToMove, counterOfCall) => {
+          while (counterOfCall) {
+            this[nameFuncToMove]();
+            --counterOfCall;
+          }
+        };
+
+        numberOfcallsToMove(
+          deltaToggleID > 0 ? 'prevSlide' : 'nextSlide',
+          Math.abs(deltaToggleID)
+        );
+
+        this.activeToggleID = selectedToggleID;
+        this.isClickOnToggle = false;
+      });
+    });
+  }
+
+  swipeMoving() {
+    this.slider.addEventListener('touchstart', (e) => {
+      e.preventDefault();
+      this.swipe.startX = e.changedTouches[0].clientX;
+      this.swipe.startTime = Date.now();
+    });
+
+    this.slider.addEventListener('touchmove', (e) => {
+      e.preventDefault();
+
+      // Різниця між поточним дотиком і стартовим дотиком;
+      this.swipe.deltaX = e.changedTouches[0].clientX - this.swipe.startX;
+    });
+
+    this.slider.addEventListener('touchend', (e) => {
+      e.preventDefault();
+
+      const duration = Date.now() - this.swipe.startTime;
+      const distance = Math.abs(this.swipe.deltaX);
+      if (
+        distance < this.swipe.minDistance ||
+        distance > this.swipe.maxDistance ||
+        duration > this.swipe.maxTime
+      ) {
+        return;
+      }
+
+      if (this.swipe.deltaX < 0) {
+        this.trottledSlideSwitch.call(this, this.prevSlide, 500);
+      } else {
+        this.trottledSlideSwitch.call(this, this.nextSlide, 500);
+      }
+
+      this.swipe.deltaX = 0;
+    });
+  }
+  //trottledSlideSwitch затримка для великії к-ті виклів ф-ції.
+  //delayMs = duration в .slider__slideList--moveTransition.
+  trottledSlideSwitch(func, delayMs) {
+    if (this.isTrottled) return;
+
+    func.call(this);
+
+    setTimeout(() => (this.isTrottled = false), delayMs);
+    this.isTrottled = true;
+  }
+
+  setParamsForScreen() {
+    if (window.innerWidth <= 768 && this.step !== 100) {
+      this.step = 100;
     }
 
-    run() {
-      this.slider.addEventListener('touchstart', (e) => {
-        //для мобільки
-        if (window.innerWidth <= 768 && this.step !== 100) {
-          this.step = 100;
-          // this.inVisibleArea = 1;
-          // this.helperBoundary = -this.inVisibleArea;
-          // this.helperCaruselSlides = [...this.slides].splice(
-          //   0,
-          //   this.inVisibleArea
-          // );
-        }
-
-        e.preventDefault();
-        this.swipe.slideWidth = this.slider.getBoundingClientRect().width;
-        this.swipe.startX = e.changedTouches[0].clientX;
-        this.removeTransitionSlider();
-        // this.swipe.startTime = Date.now();
-      });
-
-      this.slider.addEventListener('touchmove', (e) => {
-        e.preventDefault();
-        // Різниця між поточним дотиком і стартовим дотиком;
-        this.swipe.deltaX = e.changedTouches[0].clientX - this.swipe.startX;
-        // Значення різниці з пікселів в проценти;
-        this.swipe.deltaXPersentage = parseInt(
-          (this.swipe.deltaX / this.swipe.widthSlider) * 100
-        );
-        // Якщо відстань свайпу більше певної області слайдера вліво
-        if (this.swipe.deltaXPersentage < this.swipe.prevBoundary) {
-          // змінюємо слайд;
-          this.trottledSlideSwitch.call(this, this.previousSlide, 500);
-          // Моментально повертаємо слайдер і слайди в початкову позицію;
-          // Затримка необхіна для синхронизації, щоб не було "перескакувань";
-          if (this.counterShiftSlider === this.endBoundarySlider) {
-            setTimeout(() => this.carusel('prev'), 500);
-          }
-        }
-
-        // Рухаємо слайдер із слайдами при утримуванні дотику;
-        if (this.swipe.deltaXPersentage > this.swipe.prevBoundary) {
-          const step =
-            this.step * this.counterShiftSlider + this.swipe.deltaXPersentage;
-          this.setTranslateX(this.slider, step);
-        }
-
-        console.log(this.counterShiftSlider);
-      });
-
-      this.slider.addEventListener('touchend', (e) => {
-        e.preventDefault();
-        // При свайпі не було зміни слайду,
-        // повертаємо поточний слайд в початкове положення;
-        if (Math.abs(this.swipe.deltaXPersentage) < this.swipe.nextBoundary) {
-          this.addTransitionSlider();
-          this.setTranslateX(this.slider, this.step * this.counterShiftSlider);
-        }
-        // const duration = Date.now() - this.swipe.startTime;
-        // const distance = Math.abs(this.swipe.deltaX);
-        // if (
-        //   distance < this.swipe.minDistance ||
-        //   distance > this.swipe.maxDistance ||
-        //   duration > this.swipe.maxTime
-        // ) {
-        //   return;
-        // }
-
-        // if (this.swipe.deltaX < 0) {
-        //   this.trottledSlideSwitch.call(this, this.previousSlide, 500);
-        // } else {
-        //   this.trottledSlideSwitch.call(this, this.nextSlide, 500);
-        // }
-
-        // this.swipe.deltaX = 0
-      });
-
-      this.nextBtn.addEventListener(
-        'click',
-        this.trottledSlideSwitch.bind(this, this.nextSlide, 500)
-      );
-
-      this.prevBtn.addEventListener(
-        'click',
-        this.trottledSlideSwitch.bind(this, this.previousSlide, 500)
-      );
+    if (window.innerWidth >= 768 && this.step !== 70) {
+      this.step = 70;
     }
   }
 
-  const projectSlider = new Slider(sliderElems, setting);
-  projectSlider.run();
-};
+  init() {
+    this.setParamsForScreen();
+    window.addEventListener('resize', () => {
+      this.setParamsForScreen();
+    });
+  }
+  run() {
+    this.init();
+    this.toggleMoving();
+    this.swipeMoving();
+    this.nextBtn.addEventListener(
+      'click',
+      this.trottledSlideSwitch.bind(this, this.nextSlide, 500)
+    );
+    this.prevBtn.addEventListener(
+      'click',
+      this.trottledSlideSwitch.bind(this, this.prevSlide, 500)
+    );
+  }
+}
