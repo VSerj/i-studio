c5a564b2e7ebf91eb859553de1a521ad8eda5b84
SerdzhyoVashchuk <serj.vaschuk@gmail.com>
2020-11-19 04:15:42 +0200

    add: closing the menu , when click on the navlink

--- src/js/menu_btn.js
+++ src/js/menu_btn.js
@@ -1,20 +1,23 @@
-'use strict'
+'use strict';
 
 export const runBurger = () => {
-  const burger = document.querySelector('.btn-burger')
-  const nav = document.querySelector('.nav')
+  const burger = document.querySelector('.btn-burger');
+  const nav = document.querySelector('.nav');
 
   document.addEventListener('click', function (e) {
-    const { target } = e
+    const { target } = e;
 
     if (target.closest('.btn-burger')) {
-      burger.classList.toggle('is_active_menu')
-      nav.classList.toggle('is_active_menu')
+      burger.classList.toggle('is_active_menu');
+      nav.classList.toggle('is_active_menu');
     }
 
-    if (!target.closest('.nav-panel-mobile') && !target.closest('.nav')) {
-      burger.classList.remove('is_active_menu')
-      nav.classList.remove('is_active_menu')
+    if (
+      (!target.closest('.nav-panel-mobile') && !target.closest('.nav')) ||
+      target.closest('.nav__link')
+    ) {
+      burger.classList.remove('is_active_menu');
+      nav.classList.remove('is_active_menu');
     }
-  })
-}
+  });
+};
